5f6fd7a61dd0ffe2f033c949a3803f8f9d3a4db0
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-11 16:44:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+
+const inputName = document.getElementById('name');
+const inputEmail = document.getElementById('email');
+const inputPassword = document.getElementById('password');
+const inputConfirmPass = document.getElementById('confirm-password');
+const submitBtn = document.getElementById('submit');
+// alert("welcome")
+
+submitBtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    let name1 = inputName.value
+    let email2 = inputEmail.value
+    let password3 = inputPassword.value
+    let confirmPassword4 = inputConfirmPass.value
+    console.log(name1)
+
+    if(password3 !== confirmPassword4){
+        alert("Passwords do not match");
+    }else {
+        sessionStorage.setItem("username",name1);
+        sessionStorage.setItem("useremail",email2);
+        sessionStorage.setItem("userPass",password3)
+        alert("Sign up successful!")
+    }
+
+})
